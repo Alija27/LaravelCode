30f3afb450434d508a1d2932dd302a39970d083f
Alija Ghimire <alijaghimire27@gmail.com>
2022-04-22 14:34:28 +0545

    if else

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 <h1>
-    Welcome Home {{$name}}
-    Wlcome Home {!!$demo!!}
-</h1>
+    Welcome Home {{$name ?? "Viewer"}}
+   {{--  Wlcome Home {!!$demo!!} --}}
+   @if ($name=="")
+      {{ "Name is empty"}}
+       @elseif ($name=="Viewer")
+       {{"Name is correct"}}
+       @else
+       {{"Name is incorrect"}}
+       
+   @endif
+</h1>ok
