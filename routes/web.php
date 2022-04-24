81721e89f75126809be9fa30d43fee2d60b506bf
Alija Ghimire <alijaghimire27@gmail.com>
2022-04-24 08:56:46 +0545

    Route to Singleactioncontroller created

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 }); */
 
 Route::get('/', [App\Http\Controllers\DemoController::class, 'index']);
+Route::get('/courses', App\Http\Controllers\SingleActionController::class);
